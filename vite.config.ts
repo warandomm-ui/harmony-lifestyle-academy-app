93eb6ab3ebd06eacb93f285bcd1048850c842a55
warandomm-ui <warandomm@gmail.com>
2026-02-18 22:31:29 +0800

    Add Vite configuration for React project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
