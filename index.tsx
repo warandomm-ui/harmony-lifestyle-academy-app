9fa1d59d5f3f66f8fbd9690e3be9f47d4c83a680
warandomm-ui <warandomm@gmail.com>
2026-02-19 12:56:03 +0800

    Import styles.css in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ToastProvider } from './contexts/ToastContext';
